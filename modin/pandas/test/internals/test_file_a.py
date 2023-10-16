430f7f35165e1c7fdd3648a6f718ef6f1b8dc500
arunjose696 <arunjose696@gmail.com>
2023-10-16 11:43:59 +0000

    prints

--- modin/pandas/test/internals/test_file_a.py
+++ modin/pandas/test/internals/test_file_a.py
@@ -6,10 +6,10 @@ if True:
     # Initialize unidist's backend. The MPI backend is used by default.
     unidist.init()
 
-    # Apply decorator to make `square` a remote function.
-    @unidist.remote
-    def square(x):
-        return x * x
+    # # Apply decorator to make `square` a remote function.
+    # @unidist.remote
+    # def square(x):
+    #     return x * x
 
-    # Asynchronously execute remote function.
-    square_refs = [square.remote(i) for i in range(4)]
+    # # Asynchronously execute remote function.
+    # square_refs = [square.remote(i) for i in range(4)]
